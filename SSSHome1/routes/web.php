3d9f9a4ca03d43d7c0c1f28a82931c0d7cec1dc4
Sher <grant.attard70@gmail.com>
2023-12-27 20:37:18 +0000

    Task 2 Template

--- SSSHome1/routes/web.php
+++ SSSHome1/routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/Cars', [CarController::class, 'index'])->name('cars.index');
+
+Route::get('/Manufacturer', [ManufacturerController::class, 'index'])->name('manufacturer.index');
